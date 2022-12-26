78750935d41f98fe8383d7aadd5bfc915727cb23
muhammad-umar-divaaco05 <m.umerpervaiz@gmail.com>
2022-12-26 13:24:25 +0500

    Updated Code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Automated Deployment Successfully Done.
+          Hello From Version:2.0
         </p>
         <a
           className="App-link"
